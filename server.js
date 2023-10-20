f4ba35733bfad5fa1f2f9e964d528824d693a182
Ahmedouu <7medouu@gmail.com>
2023-10-20 20:31:03 +0800

    endpoint2 done

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ app.get('/api/data1',  (req, res) => {
 // endpoint 2 //this endpoint takes another files and hashes it and compares it to the hash from endpoint 1
 app.post('/api/data2', upload.single('file'), async (req, res) => {
   const fileBuffer = req.file.buffer; 
+  const fileContent = fileBuffer.toString('utf8'); // convert buffer to string
+  const newHashedContent = HASHER(fileContent); // hash the content
+  console.log(`c est chelou tu sais mira ${newHashedContent}`)
   await axios.get('http://localhost:3000/api/data1')
   .then(response => {
     console.log(response.data['hashedFile']);
@@ -42,10 +45,16 @@ app.post('/api/data2', upload.single('file'), async (req, res) => {
   .catch(error => {
     console.error('Error:', error);
   });
-  
-  console.log(req.body); 
-  res.status(201).json({ message: `File received at endpoint 2${hash}` });
+  if (newHashedContent === hash){
+    res.status(201).send('hmm the hashes do be matching')
+
+  }
+  else {
+    res.send('sans commentaire')
+  }
 });
+
+//endpoint 3// this endpoint is the craziest it takes two files and hashes both of them and tells you some info about the files 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- testImporting.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://localhost:3000/api/data1')
-  .then(response => {
-    console.log(response.data['hashedFile']);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
